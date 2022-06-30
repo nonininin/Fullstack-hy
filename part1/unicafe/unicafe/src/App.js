7cf7386cea887dda7deae1bc434a272ac1cc37a6
Aaro Angerpuro <aaro.angerpuro@gmail.com>
2022-06-30 19:34:41 +0300

    1.9

--- part1/unicafe/unicafe/src/App.js
+++ part1/unicafe/unicafe/src/App.js
@@ -1,9 +1,11 @@
 import { useState } from 'react'
 
 const App = () => {
+  const [allClicks, setAll] = useState([])
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+
 
   const Statistics = (props) => {
     return (
@@ -13,16 +15,39 @@ const App = () => {
 
   const handleGoodClick = () => {
     console.log('good')
+    setAll(allClicks + 1)
     setGood(good + 1)
   }
 
   const handleNeutralClick = () => {
     console.log('neutral')
+    setAll(allClicks + 1)
     setNeutral(neutral + 1)
   }
   const handleBadClick = () => {
     console.log('bad')
+    setAll(allClicks + 1)
     setBad(bad + 1)
+  }
+
+  const History = (props) => {
+    if (props.allClicks.length === 0) {
+      return (
+        <div>
+          No feedback given
+        </div>
+      )
+    }
+    return (
+      <div>
+        <Statistics counter={good} text={"good "} />
+        <Statistics counter={neutral} text={"neutral "} />
+        <Statistics counter={bad} text={"bad "} />
+        <Statistics counter={bad + good + neutral} text={"all "} />
+        <Statistics counter={((good - bad) / (bad + good + neutral))} text={"average "} />
+        <Statistics counter={((good) / (bad + good + neutral))} text={"positive "} />
+      </div>
+    )
   }
 
   return (
@@ -32,12 +57,8 @@ const App = () => {
       <button onClick={() => handleNeutralClick()}>neutral</button>
       <button onClick={() => handleBadClick()}>bad</button>
       <h1>statistics</h1>
-      <Statistics counter={good} text={"good "} />
-      <Statistics counter={neutral} text={"neutral "} />
-      <Statistics counter={bad} text={"bad "} />
-      <Statistics counter={bad + good + neutral} text={"all "} />
-      <Statistics counter={((good - bad) / (bad + good + neutral))} text={"average "} />
-      <Statistics counter={((good) / (bad + good + neutral))} text={"positive "} />
+      <History allClicks={allClicks} />
+
     </div>
   )
 }
